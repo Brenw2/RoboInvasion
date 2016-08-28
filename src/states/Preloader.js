efde94df019a77b0936051fa2789e04408227b68
Bren <brenw2@gmail.com>
2016-08-28 06:43:47 -0400

    add level1 tilemap, player with collison on surface and clean version of player spritesheet

--- src/states/Preloader.js
+++ src/states/Preloader.js
@@ -1,21 +1,20 @@
-Game.Preloader = function(game) {
+Game.Preloader = function (game) {
     this.preloadBar = null;
 }
-
 Game.Preloader.prototype = {
- preload: function(){
-     this.preloadBar = this.add.sprite(this.world.centerX,this.world.centerY, 'preloaderBar');
-     this.preloadBar.anchor.setTo(0.5,0.5);
-     this.time.advancedTime = true;
-     this.load.setPreloadSprite(this.preloadBar);
-     
-     //Load All Assets
-     
-     this.load.tilemap('level1Map', '/assets/levelMaps/level1.json');
-     this.load.image('tileset', '/assets/img/tileset.png')
- },
-    
-    create: function(){
+    preload: function () {
+        this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBar');
+        this.preloadBar.anchor.setTo(0.5, 0.5);
+        this.time.advancedTime = true;
+        this.load.setPreloadSprite(this.preloadBar);
+        //Load All Assets
+        this.load.image('bullet', 'assets/img/playerBullet.png', 13, 5);
+        this.load.atlasJSONHash('spaceman', '../../assets/img/playerSpacemanSpriteMap.png', '../../assets/img/playerSpacemanSpriteMap.json');
+        this.load.tilemap('map', '../../assets/levelMaps/level1Map_LandLayer.csv', null, Phaser.Tilemap.CSV);
+        this.load.tilemap('level1TileMap', '../../assets/levelMaps/level1Map.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('tiles', '../../assets/img/tileset.png');
+    }
+    , create: function () {
         this.state.start('Level');
     }
 }
